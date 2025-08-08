dd2010618027d189dfc1e466114af63daa8c8c7c
JIANG ZHUOSONG <jiangzhuosong@oppo.com>
2025-08-08 15:47:40 +0800

    [remove] delete verl/data legacy

--- verl/data/prepare.sh
+++
@@ -1,4 +0,0 @@
-python prepare_data.py /home/notebook/code/group/jiangzhuosong/opensource/Agent_Foundation_Models/verl/data/nq_full.jsonl \
--o data/wiki_search_v3 \
---tool-type single \
---sys-prompt-key WIKI_SEARCH_V3
