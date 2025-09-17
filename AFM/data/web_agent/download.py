d41cd4f8d50689f069e077055fd2e46a2e5170c2
tongyan.zjy <tongyan.zjy@alibaba-inc.com>
2025-09-17 18:22:52 +0800

    zhujiayan

--- AFM/data/web_agent/download.py
+++ AFM/data/web_agent/download.py
@@ -1,13 +1,13 @@
 from datasets import load_dataset
 
 # Login using e.g. `huggingface-cli login` to access this dataset
-# ds_sft = load_dataset("PersonalAILab/AFM-WebAgent-SFT-Dataset")
-# ds_sft.save_to_disk("./AFM-WebAgent-SFT-Dataset")
+ds_sft = load_dataset("PersonalAILab/AFM-WebAgent-SFT-Dataset")
+ds_sft.save_to_disk("./AFM-WebAgent-SFT-Dataset")
 
-# ds_rl = load_dataset("PersonalAILab/AFM-WebAgent-RL-Dataset")
-# ds_rl.save_to_disk("./AFM-WebAgent-RL-Dataset")
+ds_rl = load_dataset("PersonalAILab/AFM-WebAgent-RL-Dataset")
+ds_rl.save_to_disk("./AFM-WebAgent-RL-Dataset")
 
 
-from datasets import download_dataset
-download_dataset("amap_search_rag/AFM-WebAgent-SFT-Dataset",local_dir="./AFM-WebAgent-SFT-Dataset")
-download_dataset("amap_search_rag/AFM-WebAgent-RL-Dataset",local_dir="./AFM-WebAgent-RL-Dataset")
+# from datasets import download_dataset
+# download_dataset("amap_search_rag/AFM-WebAgent-SFT-Dataset",local_dir="./AFM-WebAgent-SFT-Dataset")
+# download_dataset("amap_search_rag/AFM-WebAgent-RL-Dataset",local_dir="./AFM-WebAgent-RL-Dataset")
