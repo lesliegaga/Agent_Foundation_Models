8d711747e222403455252ac9d6c842f10aba2209
tongyan.zjy <tongyan.zjy@alibaba-inc.com>
2025-09-22 15:53:14 +0800

    zhujiayan

--- AFM/tool_servers/wiki_server/launch_rag_server.sh
+++ AFM/tool_servers/wiki_server/launch_rag_server.sh
@@ -8,7 +8,7 @@ retriever_path=/mnt/tongyan.zjy/openlm/model/intfloat/e5-base-v2
 port=8000
 
 # Restrict to GPU4 and GPU5 only to avoid OOM on other busy GPUs
-export CUDA_VISIBLE_DEVICES=4,5
+# export CUDA_VISIBLE_DEVICES=4,5
 
 python -u wiki_rag_server.py --index_path $index_file \
                                             --corpus_path $corpus_file \
