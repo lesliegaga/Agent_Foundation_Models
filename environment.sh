d896f65a081dccc02340484569f14f9442ca4122
tongyan.zjy <tongyan.zjy@alibaba-inc.com>
2025-09-22 16:24:27 +0800

    zhujiayan

--- environment.sh
+++ environment.sh
@@ -10,34 +10,34 @@ export WIKI_RAG_SERVER_URL="http://$SERVER_HOST:8000/retrieve"
 #                                      GRM
 # =====================================================================================================================
 # for llm as judge
-export GRM_BASE_URL="https://api.uniapi.vip/v1"
-export GRM_API_KEY="api key"
-export GRM_MODEL_NAME="gpt-4.1-mini"
+export GRM_BASE_URL="http://$SERVER_HOST:8101/v1"
+export GRM_API_KEY="1234"
+export GRM_MODEL_NAME="qwen3_235b"
 # =====================================================================================================================
 #                                      Jina
 # =====================================================================================================================
 # for crawl page
 export JINA_BASE_URL="https://s.jina.ai/"
-export JINA_API_KEY="api key"
+export JINA_API_KEY="jina_9a8fc69d2a434d8d83647806d4a44f8ctGKe3Mj9u9k5DL_N5c7KLXFJSyFG"
 # =====================================================================================================================
 #                                      Serper
 # =====================================================================================================================
 # for web search
 export WEB_SEARCH_METHOD_TYPE="serapi"
 export WEB_SEARCH_SERP_NUM="10"
-export WEB_SEARCH_SERPER_API_KEY="api key"
+export WEB_SEARCH_SERPER_API_KEY="f43e9afc7ce856912b3ce3629c2ab9e1107041bb"
 # =====================================================================================================================
 #                                      Summary Model
 # =====================================================================================================================
 # for summary of crawl page content
-export SUMMARY_OPENAI_API_BASE_URL="api url"
-export SUMMARY_OPENAI_API_KEY="api key"
-export SUMMARY_MODEL="qwen2.5-72b-instruct"
+export SUMMARY_OPENAI_API_BASE_URL="http://$SERVER_HOST:8101/v1"
+export SUMMARY_OPENAI_API_KEY="1234"
+export SUMMARY_MODEL="qwen3_235b"
 # =====================================================================================================================
 #                                      Code testcases
 # =====================================================================================================================
 # for evaluate code val datasets
 export LIVECODEBENCH_DATA_PATH="$(pwd)/data/code_agent/livecodebench_testcases"
 
-export UNI_API_URLS="https://api.uniapi.vip/v1" # or other qwen3 api provider
-export UNI_API_KEY="api key"
+export UNI_API_URLS="http://$SERVER_HOST:8101/v1" # or other qwen3 api provider
+export UNI_API_KEY="1234"
