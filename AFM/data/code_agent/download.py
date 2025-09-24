5579dc539d21b1d96a7e488100ca51fd10ac32c2
tongyan.zjy <tongyan.zjy@alibaba-inc.com>
2025-09-24 17:06:40 +0800

    zhujiayan

--- AFM/data/code_agent/download.py
+++ AFM/data/code_agent/download.py
@@ -1,8 +1,14 @@
-from datasets import load_dataset
+# from datasets import load_dataset
 
-# Login using e.g. `huggingface-cli login` to access this dataset
-ds_sft = load_dataset("PersonalAILab/AFM-CodeAgent-SFT-Dataset")
-ds_sft.save_to_disk("./AFM-CodeAgent-SFT-Dataset")
+# # Login using e.g. `huggingface-cli login` to access this dataset
+# ds_sft = load_dataset("PersonalAILab/AFM-CodeAgent-SFT-Dataset")
+# ds_sft.save_to_disk("./AFM-CodeAgent-SFT-Dataset")
 
-ds_rl = load_dataset("PersonalAILab/AFM-CodeAgent-RL-Dataset")
-ds_rl.save_to_disk("./AFM-CodeAgent-RL-Dataset")
+# ds_rl = load_dataset("PersonalAILab/AFM-CodeAgent-RL-Dataset")
+# ds_rl.save_to_disk("./AFM-CodeAgent-RL-Dataset")
+
+
+
+from datasets import download_dataset
+download_dataset("amap_search_rag/AFM-WebAgent-SFT-Dataset",local_dir="./")
+download_dataset("amap_search_rag/AFM-WebAgent-RL-Dataset",local_dir="./")
