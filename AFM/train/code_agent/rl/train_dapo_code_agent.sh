0dfe91ed681909fcdf165b7dd53a6acb1f080ac9
tongyan.zjy <tongyan.zjy@alibaba-inc.com>
2025-09-25 16:17:25 +0800

    zhujiayan

--- AFM/train/code_agent/rl/train_dapo_code_agent.sh
+++ AFM/train/code_agent/rl/train_dapo_code_agent.sh
@@ -41,14 +41,17 @@ export VLLM_ATTENTION_BACKEND=XFORMERS # vllm + qwen2-7b with flash_attn has som
 # unset RAY_MEMORY
 # unset RAY_OBJECT_STORE_MEMORY
 # export RAY_DISABLE_DASHBOARD=0
-# export RAY_TMPDIR="/mnt/tongyan.zjy/tmp/ray"
+export RAY_TMPDIR="/mnt/tongyan.zjy/tmp/ray"
 # export RAY_DEDUP_LOGS=0
+# export RAY_NUM_PRESTART_WORKERS=0
+# export RAY_MAXIMUM_STARTUP_CONCURRENCY=8
+# export RAY_WORKER_REGISTER_TIMEOUT_SECONDS=120
 # 绑定到真实主机 IP，dashboard 监听 0.0.0.0，避免 agent 绑定不可达地址
 # 单机绑定回环地址，确保 raylet 与 agents 在相同地址通信，避免本机外网地址导致的拒连
-# export RAY_NODE_IP_ADDRESS="127.0.0.1"
+export RAY_NODE_IP_ADDRESS="127.0.0.1"
 # export RAY_GCS_ADDRESS="${SERVER_HOST}:6379"
 # export RAY_ADDRESS="$RAY_GCS_ADDRESS"
-# export RAY_DASHBOARD_HOST="127.0.0.1"
+export RAY_DASHBOARD_HOST="127.0.0.1"
 TRAIN_DATASETS="${CURRENT_DIR}/amap_search_rag_AFM-CodeAgent-RL-Dataset_20250924165348/CodeAgentRLDataset.parquet"   # your train dataset
 VAL_DATASETS="${CURRENT_DIR}/amap_search_rag_AFM-CodeAgent-RL-Dataset_20250924165348/CodeAgentRLDataset.parquet"
 # =====================================================================================================================
@@ -66,7 +69,7 @@ AFM_CONFIG="${CURRENT_DIR}/verl/verl/tools/config/afm_tool_config/afm_tool_confi
 cd verl
 ray stop --force >/dev/null 2>&1 || true
 # # 预启动本地 Ray head，以提升 runtime env agent 稳定性
-# ray start --head --num-cpus=16 --temp-dir="$RAY_TMPDIR" --include-dashboard=true --dashboard-host="$RAY_DASHBOARD_HOST" ${RAY_NODE_IP_ADDRESS:+--node-ip-address="$RAY_NODE_IP_ADDRESS"} | cat
+ray start --head --num-cpus=16 --temp-dir="$RAY_TMPDIR" --include-dashboard=true --dashboard-host="$RAY_DASHBOARD_HOST" ${RAY_NODE_IP_ADDRESS:+--node-ip-address="$RAY_NODE_IP_ADDRESS"} | cat
 # # 解析当前 Ray 会话的 GCS 地址（固定 6379）
 # SESSION_DIR=$(readlink -f "$RAY_TMPDIR/session_latest" 2>/dev/null || echo "")
 # if [ -n "$SESSION_DIR" ] && [ -f "$SESSION_DIR/node_ip_address.json" ]; then
