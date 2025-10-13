03ec5dbd6ecf9f7ad384975cfa6aafd690ec32d9
tongyan.zjy <tongyan.zjy@alibaba-inc.com>
2025-10-13 16:21:56 +0800

    zhujiayan

--- verl/verl/workers/sharding_manager/fsdp_vllm.py
+++ verl/verl/workers/sharding_manager/fsdp_vllm.py
@@ -279,7 +279,7 @@ class FSDPVLLMShardingManager(BaseShardingManager):
 
         patch_vllm_moe_model_weight_loader(model)
         device = get_device_id()  # used when fsdp2 set cpu_offload_policy
-        loaded_params = model.load_weights(((name, param.to(device, non_blocking=True).full_tensor()) for name, param in updated_params.items()))
+        loaded_params = model.load_weights(((name, param.to(device, non_blocking=True).full_tensor() if isinstance(param, DTensor) else param) for name, param in updated_params.items()))
 
         self.base_sync_done = True
         logger.info(f"vLLM load weights, loaded_params: {len(loaded_params) if loaded_params else -1}")
